269cd37a0ed3dc750784a21c03206e6d584f4d15
Alexandre Eberhardt <alexandre.eberhardt@etu.utc.fr>
2023-10-10 10:01:10 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tp3.h"
 #include "tp3.c"
 
 int main(){
@@ -23,22 +22,82 @@ int main(){
         printf("\n   Votre choix ? ");
 
         choix = getchar();
-
+        printf("\n");
         switch (choix) {
             case '1' :
+
+                while (ma_chaine_de_blocs){
+                    printf("n° du bloc : %d \n",ma_chaine_de_blocs->idBloc);
+                }
                 break;
 
             case '2' :
+                {
+
+                int idbloc;
+                printf("entrez le numero du bloc a afficher : ");
+                scanf("%d",&idbloc);
+                while (ma_chaine_de_blocs){
+                    if (ma_chaine_de_blocs->idBloc==idbloc){
+                        int i=0;
+                        T_Transaction *t;
+                        t=ma_chaine_de_blocs->listeTransactions;
+                        while (t){
+                            printf("transaction n°%d : le %s, %s, %lf€ \n",i,ma_chaine_de_blocs->dateBloc,t->description,t->montant);
+                            i++;
+                            t=t->suivant;
+                        }
+                    }
+                }
                 break;
+                }
 
             case '3' :
-                break;
+                {
 
+                int idetu;
+                int i;
+                printf("entrez l'id de l'etudiant a afficher : ");
+                scanf("%d",&idetu);
+                float solde = soldeEtudiant(idetu,ma_chaine_de_blocs);
+                T_Transaction *t;
+                t=ma_chaine_de_blocs->listeTransactions;
+                while (t){
+                    if (t->idEtu==idetu){
+                        printf("transaction n°%d : le %s, %s, %lf€ \n",i,ma_chaine_de_blocs->dateBloc,t->description,t->montant);
+                        i++;
+                    }
+                    t=t->suivant;
+                }
+                break;
+                }
             case '4' :
-                break;
+                {
 
-            case '5' :
+                int idetu;
+                printf("entrez l'id de l'etudiant a afficher : ");
+                scanf("%d",&idetu);
+                consulter(idetu, ma_chaine_de_blocs);
                 break;
+                }
+            case '5' :
+                {
+                int idetu;
+                float montant;
+                char desc[100];
+                printf("entrez l'id de l'etudiant a crediter : ");
+                scanf("%d",&idetu);
+                printf("\n");
+                printf("entrez le montant a crediter : ");
+                scanf("%lf",&montant);
+                printf("\n");
+                printf("entre la description : ");
+                fgets(desc, sizeof(desc), stdin);
+                printf("\n");
+                crediter(idetu, montant, desc, ma_chaine_de_blocs);
+
+                break;
+                }
 
             case '6' :
                 break;

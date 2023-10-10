09740e3d1b03bd77ef0bb0ce70cf6ca38f824d14
Alexandre Eberhardt <alexandre.eberhardt@etu.utc.fr>
2023-10-10 06:42:59 +0000

    Upload New File

---
+++ tp3.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tp3.h"
+
+
+
+
+
+/* ********************************
+ * Création et initialisation Bloc
+ ******************************** */
+T_Block *creerBloc(int id, char* date){
+    T_Block *nouveauBloc = malloc(sizeof(T_Block));
+    if (nouveauBloc != NULL) {
+        // l'allocation mémoire s'est bien passée
+        nouveauBloc->dateBloc = malloc(strlen(date) + 1);
+        strcpy(nouveauBloc->dateBloc,date);
+        nouveauBloc->idBloc = id;
+        nouveauBloc->listeTransactions = NULL;
+        nouveauBloc->suivant = NULL;
+    }
+
+    return nouveauBloc;
+}
+
+/* **************************************
+ * Création et initialisation Transaction
+ ************************************** */
+T_Transaction *creerTransaction(int id, float montant, char *descr) {
+    T_Transaction *nouvelleTransaction = malloc(sizeof(T_Transaction));
+    if (nouvelleTransaction != NULL) {
+        // l'allocation mémoire s'est bien passée
+        nouvelleTransaction->description = malloc(strlen(descr) + 1); // Important : malloc permet d'avoir l'espace mémoire dédié pour notre champ de structure
+        strcpy(nouvelleTransaction->description,descr);
+        nouvelleTransaction->idEtu = id;
+        nouvelleTransaction->montant = montant;
+        nouvelleTransaction->suivant = NULL;
+    }
+
+    return nouvelleTransaction;
+}
+
+
+/* ********************************
+ * 1.	Ajout d'une transaction en tête d'une liste de transactions :
+ ******************************** */
+T_Transaction *ajouterTransaction(int idEtu, float montant, char *descr, T_Transaction *listeTransaction) {
+    // TODO
+    return NULL;
+}
+
+
+
+/* ********************************
+ * 2.	Ajout d'un bloc en tête de la BlockChain :
+ ******************************** */
+BlockChain ajouterBlock(BlockChain bc){
+    // TODO
+    return NULL;
+}
+
+
+/* ********************************
+ * 3.	Calcul de la somme des EATCoin crédités et dépensés par un étudiant sur une journée :
+ ******************************** */
+float totalTransactionEtudiantBlock(int idEtu, T_Block b){
+    // TODO
+    return 0;
+}
+
+
+/* ********************************
+ * 4.	Calcul du solde total d'un étudiant:
+ ******************************** */
+float soldeEtudiant(int idEtu, BlockChain bc){
+    // TODO
+    return 0;
+}
+
+
+/* ********************************
+ * 5.	Rechargement du compte d’un étudiant :
+ ******************************** */
+void crediter(int idEtu, float montant, char *descr, BlockChain bc){
+    // TODO
+
+}
+
+
+/* ********************************
+ * 6.	Paiement d'un repas :
+ ******************************** */
+int payer(int idEtu, float montant, char *descr, BlockChain bc){
+    // TODO
+    return 1;
+}
+
+
+/* ********************************
+ * 7.	Historique d’un étudiant :
+ ******************************** */
+void consulter(int idEtu, BlockChain bc){
+    // TODO
+
+}
+
+
+/* ********************************
+ * 8.	Transfert de EATCoins entre deux étudiants :
+ ******************************** */
+int transfert(int idSource, int idDestination, float montant, char *descr, BlockChain bc){
+    // TODO
+    return 1;
+}
+
+
+
+
+
+
+
+/* *********************
+ * Fonctions utilitaires
+ ********************* */
+// Vider le buffer (utile quand on utlise des getchar() )
+void viderBuffer() {
+    int c = 0;
+    while (c != '\n' && c != EOF) {
+        c = getchar();
+    }
+}

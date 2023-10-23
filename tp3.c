9cf6224ac73b205afa6c74ee682baa300456198f
Lola Trupin <lola.trupin@etu.utc.fr>
2023-10-23 06:25:16 +0000

    Update tp3.c

--- tp3.c
+++ tp3.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <math.h>
 #include "tp3.h"
+//Nous définissions la date ici, le programme de base aurait été cherché la date de l'ordinateur 
 char *dateJ="20231024";
-
-//remarque pour la date : convertir en entier la chaine de char et ajouter par exemple +3 ou autre, puis revenir en char
-// on peut aussi utiliser directement la date où se positionner
 
 
 /* ********************************
@@ -15,7 +13,7 @@ char *dateJ="20231024";
 T_Block *creerBloc(int id, char* date){
     T_Block *nouveauBloc = malloc(sizeof(T_Block));
     if (nouveauBloc != NULL) {
-        // l'allocation mémoire s'est bien passée
+        //Permet de vérifier que l'allocation mémoire s'est bien passée
         nouveauBloc->dateBloc = malloc(strlen(date) + 1);
         strcpy(nouveauBloc->dateBloc,date);
         nouveauBloc->idBloc = id;
@@ -33,7 +31,7 @@ T_Transaction *creerTransaction(int id, float montant, char *descr) {
     T_Transaction *nouvelleTransaction = malloc(sizeof(T_Transaction));
     if (nouvelleTransaction != NULL) {
         // l'allocation mémoire s'est bien passée
-        nouvelleTransaction->description = malloc(strlen(descr) + 1); // Important : malloc permet d'avoir l'espace mémoire dédié pour notre champ de structure
+        nouvelleTransaction->description = malloc(strlen(descr) + 1);
         strcpy(nouvelleTransaction->description,descr);
         nouvelleTransaction->idEtu = id;
         nouvelleTransaction->montant = montant;
@@ -50,7 +48,7 @@ T_Transaction *creerTransaction(int id, float montant, char *descr) {
 T_Transaction *ajouterTransaction(int idEtu, float montant, char *descr, T_Transaction *listeTransaction) {
     T_Transaction* nouveauLT;
     nouveauLT = creerTransaction(idEtu, montant, descr);
-    //laissé exprès pour la lisibilité mais pas utile
+    //laissé exprès pour la lisibilité
     if (listeTransaction!=NULL){
         nouveauLT->suivant=listeTransaction;
     }
@@ -62,13 +60,10 @@ T_Transaction *ajouterTransaction(int idEtu, float montant, char *descr, T_Trans
 /* ********************************
  * 2.	Ajout d'un bloc en tête de la BlockChain :
  ******************************** */
-BlockChain ajouterBlock(BlockChain bc){ // ici on a un pointeur vers le premier element de la liste des blocks = bc
+BlockChain ajouterBlock(BlockChain bc){ //ici on a un pointeur vers le 1er element de la liste
     BlockChain chaine_de_blocs = malloc(sizeof(T_Block));
     T_Block nouveaubloc;
-
         if (bc == NULL){
-        //strcpy(chaine_de_blocs->dateBloc,dateJ);
-        //chaine_de_blocs->dateBloc = dateJ; //attention dateJ devra être modifiable pour le bien du test
             chaine_de_blocs = creerBloc(0, dateJ);
             chaine_de_blocs->suivant = NULL;
         } else {
@@ -76,9 +71,6 @@ BlockChain ajouterBlock(BlockChain bc){ // ici on a un pointeur vers le premier 
             chaine_de_blocs = creerBloc(chaine_de_blocs->idBloc, dateJ);
             chaine_de_blocs->suivant = bc;
         }
-    //on a besoin de la date
-    //pour déclanger un ajout de bloc, il faut vérifier que c'est une date diff du bloc precedent
-
     return chaine_de_blocs;
 }
 
@@ -120,7 +112,8 @@ void crediter(int idEtu, float montant, char*descr, BlockChain bc){
     //ici pas besoin de créer un bloc car on ajoute la transaction dans le premier bloc et pas dans un nouveau
     if (bc!=NULL){
         T_Transaction *listeTransaction = NULL;
-        listeTransaction = ajouterTransaction(idEtu,montant,descr,bc->listeTransactions);//ici on utilise le premier bloc de la liste pour ajouter la transaction car on concidère que c'est celle du jour
+        listeTransaction = ajouterTransaction(idEtu,montant,descr,bc->listeTransactions);
+        //ici on utilise le premier bloc de la liste pour ajouter la transaction car on concidère que c'est celle du jour
         bc->listeTransactions=listeTransaction;
 
     }
@@ -132,9 +125,8 @@ void crediter(int idEtu, float montant, char*descr, BlockChain bc){
  ******************************** */
 int payer(int idEtu, float montant, char *descr, BlockChain bc){
     montant=fabs(montant);
-
-    if (soldeEtudiant(idEtu, bc)>montant){//double vérification du solde
-        montant=montant*(-1.);
+    if (soldeEtudiant(idEtu, bc)>montant){//double vérification du solde mais permet d'utiliser la fonction autrement que dans le choix 6 si on le souhaitait
+        montant=montant*(-1.);//permet de rentrer le bon signe pour le montant et ainsi calculer le bon solde
         bc->listeTransactions = ajouterTransaction(idEtu,montant,descr,bc->listeTransactions);
     }
     return 0;
@@ -180,10 +172,6 @@ int transfert(int idSource, int idDestination, float montant, char *descr, Block
     }
     return 0;
 }
-
-
-
-
 
 
 

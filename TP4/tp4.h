2ff2e4a67c334423e8392ea8c33fd002d3fbd781
Alexandre Eberhardt <alexandre.eberhardt@etu.utc.fr>
2023-11-27 15:58:35 +0000

    Update tp4.h

--- TP4/tp4.h
+++ TP4/tp4.h
@@ -27,7 +27,7 @@ void afficherInscriptions(T_Arbre abr);
 void afficherInscriptionsUV(T_Arbre abr, char *code);
 void strupr_(char* s);
 void viderBuffer();
-void lireFichier(T_Arbre abr, char *nomFichier); 
+T_Arbre lireFichier(T_Arbre abr, char *nomFichier);
 T_Element*pred(T_Element *liste, char*code);
 T_Element *rechercherInscription(T_Element *liste, char* code);
 T_Element * creerInscription(char* code);

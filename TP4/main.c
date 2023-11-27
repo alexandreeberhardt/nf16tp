297be46a0d923694acc945b70f842e21d2df47de
Alexandre Eberhardt <alexandre.eberhardt@etu.utc.fr>
2023-11-27 00:32:52 +0000

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -11,17 +11,14 @@ int main()
     char *nomFichier==malloc(sizeof(100*sizeof(char)));
     // ============= MENU UTILISATEUR ============= */
     char choix = '0';
-    while (choix != '9') {
+    while (choix != '6') {
         printf("\n======================================");
         printf("\n1. Inscrire un etudiant a une UV");
         printf("\n2. Charger un fichier d'inscriptions");
         printf("\n3. Afficher tous les etudiants");
         printf("\n4. Afficher les inscrits a une UV");
         printf("\n5. Supprimer une inscription");
-        printf("\n6. ");
-        printf("\n7. ");
-        printf("\n8. ");
-        printf("\n9. Quitter");
+        printf("\n6. Quitter");
         printf("\n======================================");
         printf("\n   Votre choix ? ");
 
@@ -66,7 +63,7 @@ int main()
 
                 break;
 
-            case '9' :
+            case '6' :
 
                 break;
             default :

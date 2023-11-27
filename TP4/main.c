3e4275094ef2add817464b55c085fe9ea2cdc906
Lola Trupin <lola.trupin@etu.utc.fr>
2023-11-27 01:42:52 +0000

    Update main.c

--- TP4/main.c
+++ TP4/main.c
@@ -80,44 +80,7 @@ int main()
                 break;
 
             case '5' :
-                printf("test");
-
-                /*char *Nom1=malloc(sizeof(100*sizeof(char)));
-                char *prenom1=malloc(sizeof(100*sizeof(char)));
-                char *code1=malloc(sizeof(100*sizeof(char)));*/
-                Nom1 = "AUBIN";
-                prenom1 = "LOLA";
-                code1= "NF16";
-                //noeud = creerNoeud(nom, prenom,code);
-                //printf("test 1 %s %s %s", noeud->nom, noeud->prenom, noeud->listeInscriptions->code_uv);
-                //noeud1 = rechercherNoeud(noeud, nom, prenom);
-                //printf("%s %s %s", noeud1->nom, noeud1->prenom, noeud1->listeInscriptions->code_uv);
-                //noeud2 = NULL;
-                abr = inscrire(abr, Nom1, prenom1,code1);
-                printf("\ntest 2 %s %s %s\n", abr->nom, abr->prenom, abr->listeInscriptions->code_uv);
-
-                Nom1 = "LR";
-                prenom1 = "LINN";
-                code1= "AD04";
-                abr = inscrire(abr, Nom1, prenom1,code1);
-                printf("\ntest 3 %s %s %s\n", abr->nom, abr->prenom, abr->listeInscriptions->code_uv);
-                noeud3 = abr->filsGauche;
-                printf("\ntest 4 %s %s %s\n", noeud3->nom, noeud3->prenom, noeud3->listeInscriptions->code_uv);
-
-                Nom1 = "XAMOR";
-                prenom1 = "XALO";
-                code1= "AD04";
-                noeud3 = inscrire(abr, Nom1, prenom1,code1);
-                printf("\ntest 4 %s %s %s\n", noeud3->nom, noeud3->prenom, noeud3->listeInscriptions->code_uv);
-                noeud3 = abr->filsGauche;
-
-                printf("\ntest 5 %s %s %s\n", noeud3->nom, noeud3->prenom, noeud3->listeInscriptions->code_uv);
-                noeud3 = abr->filsGauche->filsGauche;
-
-                printf("\ntest 6 %s %s %s\n", noeud3->nom, noeud3->prenom, noeud3->listeInscriptions->code_uv);
-
-                printf("\ntete %s %s %s\n", abr->nom, abr->prenom, abr->listeInscriptions->code_uv);
-                afficherInscriptions(abr);
+                
                 break;
 
             case '9' :
